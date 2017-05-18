61f64986d6c1e2c05f74ccc33e173d0f2ebd64c0
Tu Tran <tu.tran@temple.edu>
2017-05-18 10:02:17 -0400

    POST request handling in vies function and create list item

--- lists/tests.py
+++ lists/tests.py
@@ -5,9 +5,14 @@ from django.test import TestCase
 
 class HomePageTest(TestCase):
 
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_tempate(self):
         response = self.client.get('/')
         # Instead of checking the html's content
         # we now check if home_page() function
         # returns the right template which is home.html
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode('utf-8'))
         self.assertTemplateUsed(response, 'home.html')
