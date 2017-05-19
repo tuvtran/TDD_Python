6dc4d11ce95048d3d27632b25ce2bd967f4e8bc2
Tu Tran <tu.tran@temple.edu>
2017-05-19 16:47:31 -0400

    simplified home.html, moved one test to a new class in lists/tests.py, added a new view in views.py and simplified home_page and made one addition to urls.py

--- lists/tests.py
+++ lists/tests.py
@@ -25,20 +25,14 @@ class HomePageTest(TestCase):
             'item_text': 'A new list item'
         })
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(
+            response['location'],
+            '/lists/the-only-list-in-the-world/'
+        )
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
-
-    def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        response = self.client.get('/')
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
@@ -65,3 +59,19 @@ class ItemModelTest(TestCase):
             second_saved_item.text,
             'Item the second'
         )
+
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
--- superlists/urls.py
+++ superlists/urls.py
@@ -20,4 +20,8 @@ from lists import views
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.home_page, name='home'),
+    url(
+        r'^lists/the-only-list-in-the-world/$',
+        views.view_list, name='view_list'
+    ),
 ]
