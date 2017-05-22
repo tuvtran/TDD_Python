91e6b55e30e67c358eccbf3672d7f29748edf25b
Tu Tran <tu.tran@temple.edu>
2017-05-22 16:00:38 -0400

    set STATIC_ROOT in settings and disable admin

--- superlists/urls.py
+++ superlists/urls.py
@@ -19,7 +19,7 @@ from lists import views as list_views
 from lists import urls as list_urls
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
     url(r'^$', list_views.home_page, name='home'),
     url(r'^lists/', include(list_urls)),
 ]
