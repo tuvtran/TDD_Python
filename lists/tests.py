6f63dba4dab8da05d547872c2e42a0c84ae041bf
Tu Tran <tu.tran@temple.edu>
2017-05-22 00:23:03 -0400

    URLs for forms x2, moved code in views + test, new URL

--- lists/tests.py
+++ lists/tests.py
@@ -13,27 +13,6 @@ class HomePageTest(TestCase):
         # returns the right template which is home.html
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_can_save_a_POST_request(self):
-        self.client.post('/', data={'item_text': 'A new list item'})
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_redirects_after_POST(self):
-        response = self.client.post('/', data={
-            'item_text': 'A new list item'
-        })
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(
-            response['location'],
-            '/lists/the-only-list-in-the-world/'
-        )
-
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
-
 
 class ItemModelTest(TestCase):
 
@@ -75,3 +54,23 @@ class ListViewTest(TestCase):
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+
+
+class NewListTest(TestCase):
+
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={
+            'item_text': 'A new list item'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(
+            response['location'],
+            '/lists/the-only-list-in-the-world/'
+        )
--- superlists/urls.py
+++ superlists/urls.py
@@ -20,8 +20,10 @@ from lists import views
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.home_page, name='home'),
+    url(r'^lists/new$', views.new_list, name='new_list'),
     url(
         r'^lists/the-only-list-in-the-world/$',
-        views.view_list, name='view_list'
+        views.view_list,
+        name='view_list',
     ),
 ]
