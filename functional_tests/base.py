fe2abb053eb71cb9556a4b5483e118f7cee3646a
Tu Tran <tu.tran@temple.edu>
2017-05-25 10:10:43 -0400

    Add helper method wait_for() for FT

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -31,3 +31,13 @@ class FunctionalTest(StaticLiveServerTestCase):
                 if time.time() - start_time > MAX_WAIT:
                     raise e
                 time.sleep(0.5)
+
+    def wait_for(self, fn):
+        start_time = time.time()
+        while True:
+            try:
+                return fn()
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
