dabc779e7759de6d28b3c83b6a747c52f7258b58
Tu Tran <tu.tran@temple.edu>
2017-05-30 16:10:03 -0400

    use form in all views, back to working state

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -6,11 +6,6 @@ from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 
 class ItemFormTest(TestCase):
 
-    @skip
-    def test_form_renders_text_input(self):
-        form = ItemForm()
-        self.fail(form.as_p())
-
     def test_form_item_input_has_placeholder_and_css_classes(self):
         form = ItemForm()
         self.assertIn('placeholder="Enter a to-do item"', form.as_p())
--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,3 @@
-from django.core.exceptions import ValidationError
 from django.shortcuts import render, redirect
 from lists.models import Item, List
 from lists.forms import ItemForm
@@ -9,29 +8,24 @@ def home_page(request):
 
 
 def new_list(request):
-    list_ = List.objects.create()
-    item = Item(text=request.POST['text'], list=list_)
-    try:
-        item.full_clean()
-        item.save()
-    except ValidationError:
-        list_.delete()
-        error = "You can't have an empty list item"
-        return render(request, 'home.html', {"error": error})
-    return redirect(list_)
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
+        list_ = List.objects.create()
+        Item.objects.create(text=request.POST['text'], list=list_)
+        return redirect(list_)
+    else:
+        return render(request, 'home.html', {'form': form})
 
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    error = None
-
+    form = ItemForm()
     if request.method == 'POST':
-        try:
-            item = Item(text=request.POST['text'], list=list_)
-            item.full_clean()
-            item.save()
+        form = ItemForm(data=request.POST)
+        if form.is_valid():
+            Item.objects.create(text=request.POST['text'], list=list_)
             return redirect(list_)
-        except ValidationError:
-            error = "You can't have an empty list item"
 
-    return render(request, 'list.html', {'list': list_, 'error': error})
+    return render(request, 'list.html', {
+        'list': list_, "form": form
+    })
