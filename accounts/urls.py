3e64111e91c171c6736de7d031cc1f7e2ed50149
Tu Tran <tu.tran@temple.edu>
2017-06-06 15:49:23 -0400

    Forgot ^ before logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,5 @@ from accounts import views
 urlpatterns = [
     url(r'^send_login_email$', views.send_login_email, name='send_login_email'),
     url(r'^login', views.login, name='login'),
-    url(r'logout$', logout, {'next_page': '/'}, name='logout'),
+    url(r'^logout$', logout, {'next_page': '/'}, name='logout'),
 ]
