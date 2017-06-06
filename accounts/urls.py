119eaf676f808fe6dcb0e9408201b578fe2fe3e1
Tu Tran <tu.tran@temple.edu>
2017-06-06 15:14:51 -0400

    Add logout link in template; add logout url in urls.py; use django's builtin logout view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import logout
 from django.conf.urls import url
 from accounts import views
 
@@ -5,4 +6,5 @@ from accounts import views
 urlpatterns = [
     url(r'^send_login_email$', views.send_login_email, name='send_login_email'),
     url(r'^login', views.login, name='login'),
+    url(r'logout$', logout, {'next_page': '/'}, name='logout'),
 ]
